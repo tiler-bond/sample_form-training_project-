d4f6428a19687a78d47b60ac8a4696ebd1887d64
shubham dhama <shubhamdhama@SHUBHAMs-MacBook-Pro.local>
2021-10-09 16:58:54 +0530

    added code for task 3

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 'use strict';
+
+//   TASK 2
+
 let num = '';
 function alertt(number) {
   if (number === 'AC') {
@@ -8,7 +11,7 @@ function alertt(number) {
     return;
   }
   if (number === '=') {
-    showNumber(num);
+    // showNumber(num);
     showResult(eval(num));
     return;
   }
@@ -23,4 +26,46 @@ function showNumber(number) {
 }
 function showResult(num) {
   document.querySelector('.finalResult').innerHTML = num;
+}
+
+//  TASK 3
+
+const inputNumber = document.querySelector('#evenNum');
+const outerForm = document.querySelector('.evenNumbers');
+const container = document.querySelector('.container');
+const descriptionDiv = document.querySelector('.descriptionDiv');
+let html = '';
+outerForm.addEventListener('submit', notSubmittingForm);
+function notSubmittingForm(e) {
+  e.preventDefault();
+  container.innerHTML = '';
+  descriptionDiv.innerHTML = '';
+  const arr = [];
+  const number = +inputNumber.value;
+  if (number > 500) {
+    inputNumber.value = '';
+    alert('enter a valid number that should be less than 500');
+    return;
+  }
+  descriptionDiv.insertAdjacentHTML(
+    'afterbegin',
+    `<p>ALL the Even Numbers upto ${number}</p>`
+  );
+  for (let i = 1; i <= number; i++) {
+    if (i % 2 === 0) arr.push(i);
+    else continue;
+  }
+  let id = 0;
+  arr.forEach((r, i) => {
+    html = `<ul class="listNumbers" ><li>${r}</li></ul>`;
+    if (i % 15 === 0) {
+      id++;
+      const row = document.createElement('div');
+      row.setAttribute('class', 'row');
+      row.setAttribute('id', `row${id}`);
+      container.appendChild(row);
+    }
+    document.querySelector(`#row${id}`).insertAdjacentHTML('beforeend', html);
+    html = '';
+  });
 }
