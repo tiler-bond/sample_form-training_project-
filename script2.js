badfc6742d60d4eb7b2dc8912460981e63ed1879
shubham dhama <shubhamdhama@SHUBHAMs-MacBook-Pro.local>
2021-11-03 10:28:45 +0530

    code for task 7 added

--- script2.js
+++ script2.js
@@ -1,1 +1,51 @@
 'use strict';
+
+const addButton1 = document.querySelector('#add-item-1');
+const addButton2 = document.querySelector('#add-item-2');
+const addButton3 = document.querySelector('#add-item-3');
+const showBill = document.querySelector('.bill-list');
+const total = document.querySelector('.total');
+
+addButton1.addEventListener('click', function () {
+  const li = document.createElement('li');
+  const itemCost = document.createElement('span');
+  itemCost.setAttribute('class', 'price');
+  itemCost.innerHTML = `${this.dataset.cost}`;
+  li.innerHTML = `${this.dataset.name} `;
+  li.appendChild(itemCost);
+  showBill.appendChild(li);
+});
+
+addButton2.addEventListener('click', function () {
+  const li = document.createElement('li');
+  const itemCost = document.createElement('span');
+  itemCost.setAttribute('class', 'price');
+  itemCost.innerHTML = `${this.dataset.cost}`;
+  li.innerHTML = `${this.dataset.name} `;
+  li.appendChild(itemCost);
+  showBill.appendChild(li);
+});
+
+addButton3.addEventListener('click', function () {
+  const li = document.createElement('li');
+  const itemCost = document.createElement('span');
+  itemCost.setAttribute('class', 'price');
+  itemCost.innerHTML = `${this.dataset.cost}`;
+  li.innerHTML = `${this.dataset.name} `;
+  li.appendChild(itemCost);
+  showBill.appendChild(li);
+});
+
+total.addEventListener('click', function () {
+  const x = document.querySelectorAll('.price');
+  let sum = 0;
+  for (let i = 0; i < x.length; i++) {
+    sum += Number(x[i].innerHTML);
+  }
+  console.log(sum);
+  const total = sum + (18 / 100) * sum;
+  console.log(total);
+  const displayTotal = document.createElement('p');
+  displayTotal.innerHTML = `your total is ${total}  (includes 18% GST) `;
+  showBill.appendChild(displayTotal);
+});
