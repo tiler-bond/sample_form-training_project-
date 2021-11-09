2056e481cd1505161c4d9231fe40b71d71158d59
shubham dhama <shubhamdhama@SHUBHAMs-MacBook-Pro.local>
2021-11-09 12:39:05 +0530

    zomato counter code added

--- script2.js
+++ script2.js
@@ -1,43 +1,16 @@
 'use strict';
 
-const addButton1 = document.querySelector('#add-item-1');
-const addButton2 = document.querySelector('#add-item-2');
-const addButton3 = document.querySelector('#add-item-3');
 const showBill = document.querySelector('.bill-list');
 const total = document.querySelector('.total');
-
-addButton1.addEventListener('click', function () {
-  const li = document.createElement('li');
-  const itemCost = document.createElement('span');
-  itemCost.setAttribute('class', 'price');
-  itemCost.innerHTML = `${this.dataset.cost}`;
-  li.innerHTML = `${this.dataset.name} `;
-  li.appendChild(itemCost);
-  showBill.appendChild(li);
-});
-
-addButton2.addEventListener('click', function () {
-  const li = document.createElement('li');
-  const itemCost = document.createElement('span');
-  itemCost.setAttribute('class', 'price');
-  itemCost.innerHTML = `${this.dataset.cost}`;
-  li.innerHTML = `${this.dataset.name} `;
-  li.appendChild(itemCost);
-  showBill.appendChild(li);
-});
-
-addButton3.addEventListener('click', function () {
-  const li = document.createElement('li');
-  const itemCost = document.createElement('span');
-  itemCost.setAttribute('class', 'price');
-  itemCost.innerHTML = `${this.dataset.cost}`;
-  li.innerHTML = `${this.dataset.name} `;
-  li.appendChild(itemCost);
-  showBill.appendChild(li);
-});
+// const counter = document.querySelector('.Counter');
+// const addButton = document.querySelector('.initial-button');
+// const minusButton = document.querySelector('.minus-button');
+// const plusButton = document.querySelector('.plus-button');
+// const quantity = document.querySelector('.quantity');
 
 total.addEventListener('click', function () {
   const x = document.querySelectorAll('.price');
+  if (x.length === 0) return;
   let sum = 0;
   for (let i = 0; i < x.length; i++) {
     sum += Number(x[i].innerHTML);
@@ -49,3 +22,49 @@ total.addEventListener('click', function () {
   displayTotal.innerHTML = `your total is ${total}  (includes 18% GST) `;
   showBill.appendChild(displayTotal);
 });
+
+function addFood(food, price, obj) {
+  const x = obj.closest('.add-button');
+  obj.classList.add('hidden');
+  x.querySelector('.Counter').classList.remove('hidden');
+  const li = document.createElement('li');
+  li.setAttribute('class', 'bill-List');
+  li.style.listStyle = 'decimal';
+  const itemName = document.createElement('span');
+  const itemCost = document.createElement('span');
+  const itemQuantity = document.createElement('span');
+  itemCost.setAttribute('class', 'price');
+  itemName.setAttribute('class', 'item-name');
+  itemQuantity.setAttribute('class', 'item-quantity');
+  itemCost.innerHTML = price;
+  itemName.innerHTML = food;
+  itemQuantity.innerHTML = 1;
+  li.appendChild(itemName);
+  li.appendChild(itemQuantity);
+  li.appendChild(itemCost);
+  showBill.appendChild(li);
+}
+
+function plus(obj) {
+  const x = obj.closest('.Counter');
+  const y = x.querySelector('.quantity');
+  let a = Number(y.innerHTML);
+  y.innerHTML = '';
+  a++;
+  y.innerHTML = a;
+}
+
+function minus(obj) {
+  const x = obj.closest('.Counter');
+  const y = x.querySelector('.quantity');
+  let a = Number(y.innerHTML);
+  y.innerHTML = '';
+  a--;
+  y.innerHTML = a;
+  if (Number(y.innerHTML) === 0) {
+    x.classList.add('hidden');
+    const b = obj.closest('.add-button');
+    b.querySelector('.initial-button').classList.remove('hidden');
+    y.innerHTML = 1;
+  }
+}
